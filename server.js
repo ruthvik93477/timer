9b7206e39ff9a1faeeecc33c4b0d8fd2ee5f3360
Ruthvik Kanagani <128290107+ruthvik93477@users.noreply.github.com>
2024-03-08 12:29:43 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const fs = require('fs');
+const ejs = require('ejs');
+const cors = require('cors');
+const path = require('path');
+const bodyParser = require('body-parser');
+const session = require('express-session');
+require('dotenv').config();
+const app = express();
+const port = process.env.PORT;
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static('public'));
+app.use(bodyParser.json());
+const conn = process.env.MONGO_URL;
+mongoose.connect(conn);
+const userName = process.env.USER_NAME;
+const pass = process.env.PASSWORD;
+
+
+const inputSchema = new mongoose.Schema({
+    cn: String,
+    month: String,
+    name: String,
+    workedDays: String,
+    paymentStatus: String,
+    paidBy: String,
+    payDate: String,
+    remarks: String,
+})
+
+const InputData = mongoose.model('InputData',inputSchema);
+
+app.get('/',(req,res)=>{
+  res.sendFile(__dirname+'/public/home.html');
+})
+
+app.get('/upload',(req,res)=>{
+    res.sendFile(__dirname+'/public/upload.html')
+})
+
+app.post('/upload',async(req,res)=>{
+  let {cn,month,name,workedDays,paymentStatus,paidBy,payDate,remarks} = req.body;
+  try{
+    const inputData = new InputData({
+        cn: cn,
+        month: month,
+        name: name,
+        workedDays: workedDays,
+        paymentStatus: paymentStatus,
+        paidBy: paidBy,
+        payDate: payDate,
+        remarks: remarks,
+    });
+    await inputData.save();
+    let a45 = fs.readFileSync('public/submit.html')
+    res.send(a45.toString());
+  }
+  catch(err){
+    console.error(err);
+    res.status(500).send('Internal Server Error');
+  }
+})
+
+app.get('/display',async (req, res) => {
+  try {
+    const data = await InputData.find();
+    res.render('display', { data });
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.post('/update/:id',async (req, res) => {
+  try {
+    const { id } = req.params;
+    let { cn,month,name,workedDays,paidBy,paymentStatus,payDate,remarks } = req.body;
+    await InputData.findByIdAndUpdate(id, { cn,month,name,workedDays,paidBy,paymentStatus,payDate,remarks });
+    res.status(200).send('Data updated successfully');
+  } catch (error) {
+    res.status(500).send(error.message);
+  }
+});
+
+app.listen(port,()=>{
+  console.log(`server runnig at ${port}`)
+})
